7fc5a9fd63f94a09ef02a65b7c7faac4e64d3f88
doom2053 <31608217+doom2053@users.noreply.github.com>
2017-12-24 15:44:07 -0800

    Add files via upload

---
+++ Exercise 4/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculatebtn_Click(object sender, EventArgs e)
+        {
+
+            //Declaring Variables
+            int totalSeconds;
+            int hours;
+            int minutes;
+            int days;
+            int minutesRemainder;
+            int hoursRemainderMinutes;
+            int hoursRemainderSeconds;
+
+            // Parsing and calculations
+
+            totalSeconds = int.Parse(textconvert.Text);
+            minutes = totalSeconds / 60;
+            minutesRemainder = totalSeconds % 60;
+            hours = minutes / 60;
+            hoursRemainderMinutes = minutes % 60;
+            hoursRemainderSeconds = hoursRemainderMinutes % 60;
+            days = hours / 24;
+
+            if (totalSeconds >= 86400)
+            {
+                MessageBox.Show(days.ToString() + " day(s)");
+            }
+            else if (totalSeconds >= 3600)
+            {
+                MessageBox.Show(hours.ToString() + " hour(s) " );
+            }
+            else if (totalSeconds >= 60)
+            {
+                MessageBox.Show(minutes.ToString() + " minute(s) " );
+            }
+            else
+            {
+                MessageBox.Show(totalSeconds.ToString());
+            }
+        }
+    }
+}
