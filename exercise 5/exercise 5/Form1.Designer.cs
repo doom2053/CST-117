181694b15b2781a04b5d56cda77eb00fa45416b0
doom2053 <31608217+doom2053@users.noreply.github.com>
2018-01-10 20:23:13 -0800

    Add files via upload

---
+++ exercise 5/exercise 5/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace exercise_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.enterBox = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.piLabel = new System.Windows.Forms.Label();
+            this.actualLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.termLbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number of Terms:";
+            // 
+            // enterBox
+            // 
+            this.enterBox.Location = new System.Drawing.Point(202, 42);
+            this.enterBox.Name = "enterBox";
+            this.enterBox.Size = new System.Drawing.Size(128, 20);
+            this.enterBox.TabIndex = 1;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(12, 96);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(100, 78);
+            this.calculateBtn.TabIndex = 2;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // piLabel
+            // 
+            this.piLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.piLabel.Location = new System.Drawing.Point(51, 277);
+            this.piLabel.Name = "piLabel";
+            this.piLabel.Size = new System.Drawing.Size(279, 34);
+            this.piLabel.TabIndex = 3;
+            // 
+            // actualLbl
+            // 
+            this.actualLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actualLbl.Location = new System.Drawing.Point(95, 193);
+            this.actualLbl.Name = "actualLbl";
+            this.actualLbl.Size = new System.Drawing.Size(207, 23);
+            this.actualLbl.TabIndex = 4;
+            this.actualLbl.Click += new System.EventHandler(this.actualLbl_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Actual:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Calculated using";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // termLbl
+            // 
+            this.termLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.termLbl.Location = new System.Drawing.Point(156, 237);
+            this.termLbl.Name = "termLbl";
+            this.termLbl.Size = new System.Drawing.Size(68, 23);
+            this.termLbl.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(221, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "terms:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 345);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.termLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.actualLbl);
+            this.Controls.Add(this.piLabel);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.enterBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate Pi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox enterBox;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label piLabel;
+        private System.Windows.Forms.Label actualLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label termLbl;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ exercise 5/exercise 5/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercise_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actualLbl.Text = Math.PI.ToString();
+        }
+
+        private void CalculateBtn_Click(object sender, EventArgs e)
+        {
+            // Calculate Pi.
+               
+                
+
+                double pi_over_4 = 0;
+                int num_terms = int.Parse(enterBox.Text);
+                double sign = 1;
+                for (int term = 0; term < num_terms; term++)
+                {
+                    Console.WriteLine(sign + " / " + (term * 2 + 1) + " = " + (1.0 / (term * 2 + 1)));
+                    pi_over_4 += sign / (term * 2 + 1);
+                    sign *= -1;
+                }
+
+                // Display the result.
+                double pi = 4 * pi_over_4;
+            piLabel.Text = pi.ToString();
+            termLbl.Text = enterBox.Text;
+
+            }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actualLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
