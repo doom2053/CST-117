724235f63e066db37a0477836253828ad79523fb
doom2053 <31608217+doom2053@users.noreply.github.com>
2018-01-26 19:05:02 -0800

    Add files via upload

---
+++ exercise9final2/exercise9final2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+//Corrected by Mark Piland
+namespace exercise9final2
+{
+        class Program
+    {
+        static void Main( string[] args )
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+            //needed for the foreach
+            var C = A.Union(B);
+                
+            
+
+            //put some stuff in the sets
+            Random r = new Random( );
+            for(int i = 0;  i <10; i++)
+            {
+                //Needs a base of a 0 for minimum number
+                //A.addElement( r.Next(  4 ) );
+                //B.addElement( r.Next(  12 ) );
+                A.addElement(r.Next(0,4));
+                B.addElement(r.Next(0,12));
+            }
+
+            
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            //Only repeats Set B
+            //Console.WriteLine("A union B: " + C);
+            Console.WriteLine("A union B: ");
+            foreach (var n in C)
+            {
+                Console.WriteLine(n);
+            }
+
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+           
+        }
+    }
+
+}
+
+    
---
+++ exercise9final2/exercise9final2/Set.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//corrected by Mark Piland
+namespace exercise9final2
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                
+                if (val == elements[i])
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set Union(Set rhs)
+        {
+            //This causes the i interval to add more elements after the union
+            for (int i = 0; i < rhs.elements.Count; i++)
+            
+            {
+                this.addElement(rhs.elements[i]);
+            }
+            return rhs;
+        }
+    }
+
+}
